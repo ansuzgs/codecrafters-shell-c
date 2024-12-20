0b53aeabed99ed0129b94fd009293bf1bbbcb0ab
Pablo Gonzalez Santatecla <paos@gmv.es>
2024-12-20 12:43:06 +0100

    REPL

--- app/main.c
+++ app/main.c
@@ -6,12 +6,16 @@ int main() {
   setbuf(stdout, NULL);
 
   // Uncomment this block to pass the first stage
-  printf("$ ");
+  // printf("$ ");
 
   // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
+  while(1) {
+	  memset(input, 0, 100);
+	  printf("$ ");
+	  fgets(input, 100, stdin);
+	  input[strlen(input) - 1] = '\0';
+	  printf("%s: command not found\n", input);
+  }
   return 0;
 }

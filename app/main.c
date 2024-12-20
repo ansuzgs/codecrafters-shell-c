a6c607faf017b3757f37a81cef2564a0b3e1270f
Pablo Gonzalez Santatecla <paos@gmv.es>
2024-12-20 13:12:20 +0100

    type builtin

--- app/main.c
+++ app/main.c
@@ -2,28 +2,36 @@
 #include <string.h>
 
 int main() {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+	// Flush after every printf
+	setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  // printf("$ ");
+	// Uncomment this block to pass the first stage
+	// printf("$ ");
 
-  // Wait for user input
-  char input[100];
-  while(1) {
-	  memset(input, 0, 100);
-	  printf("$ ");
-	  fgets(input, 100, stdin);
-	  input[strlen(input) - 1] = '\0';
+	// Wait for user input
+	char input[100];
+	while(1) {
+		memset(input, 0, 100);
+		printf("$ ");
+		fgets(input, 100, stdin);
+		input[strlen(input) - 1] = '\0';
 
-	  if (strcmp(input, "exit 0") == 0) {
-		break;
-	  } else if (strncmp(input, "echo", 4) == 0) {
-	  	char *p = input + 5;
-		printf("%s\n", p);
-	  } else {
-		  printf("%s: command not found\n", input);
-	  }
-  }
-  return 0;
+		if (strcmp(input, "exit") == 0) {
+			break;
+		} else if (strncmp(input, "echo", 4) == 0) {
+			char *p = input + 5;
+			printf("%s\n", p);
+		} else if (strncmp(input, "type", 4) == 0) {
+			char *p = input + 5;
+			if (strncmp(p, "echo", 4) == 0) printf("echo is a shell builtin\n");
+			else if (strncmp(p, "exit", 4) == 0) printf("exit is a shell builtin\n");
+			else if (strncmp(p, "type", 4) == 0) printf("type is a shell builtin\n");
+			else {
+				printf("%s: not found\n", p); 
+			}
+		} else {
+			printf("%s: command not found\n", input);
+		}
+	}
+	return 0;
 }
